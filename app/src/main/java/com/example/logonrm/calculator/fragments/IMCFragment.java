9d55f3740b838119832e97b45b78f02dc5107582
logonrm <lagmat2002@yahoo.com.br>
2017-12-13 22:58:08 -0200

    Sem calculo

--- app/src/main/java/com/example/logonrm/calculator/fragments/IMCFragment.java
+++ app/src/main/java/com/example/logonrm/calculator/fragments/IMCFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.logonrm.calculator.R;
 
@@ -20,11 +21,16 @@ public class IMCFragment extends Fragment {
     }
 
 
+    //Fazendo o bind da altura. OBS: Falta fazer do resto
+    private EditText edt_altura;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_imc, container, false);
+        View v = inflater.inflate(R.layout.fragment_imc, container,false);
+        edt_altura = (EditText) v.findViewById(R.id.edt_altura);
+        return v;
     }
 
 }
